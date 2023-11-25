8660952af5b13fab53755c6b82804186cc0c03df
zumma009 <152015264+zumma009@users.noreply.github.com>
2023-11-25 21:37:30 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_11.0"
+export DT_LINK="https://github.com/aihara87/android_device_infinix_Infinix-X663B.git -b 11"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="Infinix-X663B"
+export OEM="infinix"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
 ## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
 ## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
-export TARGET="recoveryimage"
+export TARGET="bootimage"
 
 export OUTPUT="OrangeFox*.zip"
 
